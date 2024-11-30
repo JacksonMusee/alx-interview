6215fdce044d980f2e291b2b8dcdc3b1f6021ebf
JacksonMusee <jacksonmutisya94@gmail.com>
2024-11-30 10:49:04 +0300

    Complete task 0

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -6,32 +6,14 @@
 def makeChange(coins, total):
     """Find the minimum number of coins to make total
     """
-    '''
     if total <= 0:
         return 0
 
-    memo = {0: 0}
-
-    for i in range(1, total + 1):
-        for coin in coins:
-            subproblem = i - coin
-
-            if subproblem in memo:
-                current_best = memo.get(i)
-                if current_best:
-                    memo[i] = min(current_best, memo.get(subproblem) + 1)
-                else:
-                    memo[i] = memo.get(subproblem) + 1
-                    
-
-    return memo.get(total, -1)
-'''
-
-    dp = [float('inf')] * (total + 1)
-    dp[0] = 0
+    memo = [float('inf')] * (total + 1)
+    memo[0] = 0
 
     for coin in coins:
         for i in range(coin, total + 1):
-            dp[i] = min(dp[i], dp[i - coin] + 1)
+            memo[i] = min(memo[i], memo[i - coin] + 1)
 
-    return dp[total] if dp[total] != float('inf') else -1
+    return memo[total] if memo[total] != float('inf') else -1
